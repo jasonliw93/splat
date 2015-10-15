b5f475ab9f4e3db72788cd3ede6f50093c917ac6
Jason Liw <jasonliw93@gmail.com>
2015-10-15 14:05:27 -0400

    changed image scaling

--- public/js/views/movieposter.js
+++ public/js/views/movieposter.js
@@ -11,8 +11,8 @@ splat.MoviePoster = Backbone.View.extend({
     },
     events: {
         "change #selectImage": "selectImage",
-        "dragover #imageCanvas": "dragoverHandler",
-        "drop #imageCanvas": "dropHandler",
+        "dragover #detailsImage": "dragoverHandler",
+        "drop #detailsImage": "dropHandler",
     },
     selectImage: function(event) {
         // set object attribute for image uploader
@@ -28,7 +28,12 @@ splat.MoviePoster = Backbone.View.extend({
         var reader = new FileReader();
         // callback for when read operation is finished
         reader.onload = function(event) {
-            self.setCanvasImage(reader.result);
+            var targetImgElt = $('#detailsImage')[0];
+            targetImgElt.src = self.resize(reader.result);
+            self.model.set('poster', targetImgElt.src);
+            splat.utils.showNotice('Note!',
+            'Movie Poster udated, to make changes permanent, click "Save Changes" button', 'alert-info');
+            //self.setCanvasImage(reader.result);
         };
         reader.readAsDataURL(pictureFile); // read image file
     },
@@ -55,17 +60,28 @@ splat.MoviePoster = Backbone.View.extend({
         }
         // else display notification error
     },
-    setCanvasImage: function(image) {
-        var canvas = this.$('#imageCanvas')[0]
-        var ctx = canvas.getContext('2d');
-        var img = new Image();
-        var self = this;
-        img.onload = function() {
-            ctx.drawImage(img, 0, 0, img.width, img.height,
-                0, 0, canvas.width, canvas.height);
-            self.model.set('poster', canvas.toDataURL());
-        };
-        img.src = image;
+    // Resize sourceImg, returning result as a DataURL value. Type,
+    // quality are optional params for image-type and quality setting
+    resize: function(sourceImg, type, quality) {
+        var type = type || "image/jpeg"; // default MIME image type
+        var quality = quality || "0.95"; // tradeoff quality vs size
+        var image = new Image(), MAX_HEIGHT = 300, MAX_WIDTH = 450, x = 0, y = 0;
+        image.src = sourceImg;
+        if (image.width / image.height > MAX_WIDTH / MAX_HEIGHT) {
+            image.height = image.height * MAX_WIDTH / image.width;
+            image.width = MAX_WIDTH; //canvas.width / img.width 225px to 450 then scale is 2
+            y = (MAX_HEIGHT - image.height) / 2;
+        }else{
+            image.width = image.width * MAX_HEIGHT / image.height; //canvas.width / img.width 225px to 450 then scale is 2
+            image.height = MAX_HEIGHT;
+            x = (MAX_WIDTH - image.width) / 2;
+        }
+        var canvas = document.createElement("canvas");
+        canvas.width = MAX_WIDTH;
+        canvas.height = MAX_HEIGHT;
+        var ctx = canvas.getContext("2d"); // get 2D rendering context
+        ctx.drawImage(image,x,y, image.width, image.height); // render
+        return canvas.toDataURL(type, quality);
     },
     // render the View
     render: function() {
@@ -74,7 +90,7 @@ splat.MoviePoster = Backbone.View.extend({
         this.moviePosterLoad.done(function(markup) {
             self.moviePosterTemplate = _.template(markup);
             self.$el.html(self.moviePosterTemplate());
-            self.setCanvasImage(self.model.get('poster'));
+            $('#detailsImage').attr("src", self.model.get('poster'));
         });
         return this; // support method chaining
     }
